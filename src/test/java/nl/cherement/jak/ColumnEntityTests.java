6bc3fee425ecfdb27207998d2e112ebc498c2402
Nickmanbear <misterkees@hotmail.com>
2020-05-21 22:25:01 +0200

    all Entities tests

---
+++ src/test/java/nl/cherement/jak/ColumnEntityTests.java
@@ -0,0 +1,32 @@
+package nl.cherement.jak;
+
+import nl.cherement.jak.entity.ColumnEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class ColumnEntityTests {
+
+    private final ColumnEntity columnEntity = new ColumnEntity();
+
+    @Test
+    void id() {
+        columnEntity.setId(1);
+        assertEquals(1, columnEntity.getId());
+    }
+
+    @Test
+    void userId() {
+        columnEntity.setBoardId(1);
+        assertEquals(1, columnEntity.getBoardId());
+    }
+
+    @Test
+    void name() {
+        columnEntity.setName("ColumnTest");
+        assertEquals("ColumnTest", columnEntity.getName());
+    }
+
+}
---
+++ src/test/java/nl/cherement/jak/UserEntityTests.java
@@ -0,0 +1,65 @@
+package nl.cherement.jak;
+
+import nl.cherement.jak.entity.UserEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class UserEntityTests {
+
+    private final UserEntity userEntity = new UserEntity();
+
+    @Test
+    void id() {
+        userEntity.setId(1);
+        assertEquals(1, userEntity.getId());
+    }
+
+    @Test
+    void userName() {
+        userEntity.setUsername("UserTest");
+        assertEquals("UserTest", userEntity.getUsername());
+    }
+
+    @Test
+    void password() {
+        userEntity.setPassword("PasswordTest");
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        assertEquals(bCryptPasswordEncoder.encode("PasswordTest"), userEntity.getPassword());
+    }
+
+    @Test
+    void roles() {
+        userEntity.setRoles("GUEST,ADMIN");
+        assertEquals("GUEST,ADMIN", userEntity.getRoles());
+    }
+
+    @Test
+    void roleList() {
+        userEntity.setRoles("GUEST,ADMIN");
+        assertEquals(Arrays.asList("GUEST", "ADMIN"), userEntity.getRoleList());
+    }
+
+    @Test
+    void permissions() {
+        userEntity.setPermissions("READ,WRITE");
+        assertEquals("READ,WRITE", userEntity.getRoles());
+    }
+
+    @Test
+    void permissionList() {
+        userEntity.setPermissions("READ,WRITE");
+        assertEquals(Arrays.asList("READ", "WRITE"), userEntity.getPermissionList());
+    }
+
+    @Test
+    void active() {
+        userEntity.setActive(1);
+        assertEquals(1, userEntity.getActive());
+    }
+}
