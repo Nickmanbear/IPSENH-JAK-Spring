8362cc5dd901e9ce51e5990df7ddaa467e69f535
Nickmanbear <misterkees@hotmail.com>
2020-05-24 14:29:24 +0200

    Principal tests

--- src/test/java/nl/cherement/jak/entity/UserPrincipalTests.java
+++ src/test/java/nl/cherement/jak/entity/UserPrincipalTests.java
@@ -2,18 +2,77 @@ package nl.cherement.jak.entity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class UserPrincipalTests {
 
-    private final UserPrincipal userPrincipal = new UserPrincipal(new UserEntity());
+    private UserPrincipal userPrincipal = new UserPrincipal(new UserEntity());
 
     @Test
-    void getAuthorities() {
+    void getAuthoritiesEmpty() {
+        UserEntity userEntity = new UserEntity();
+        userPrincipal = new UserPrincipal(userEntity);
+
         assertEquals(new ArrayList<>(), userPrincipal.getAuthorities());
+    }
+
+    @Test
+    void getAuthoritiesPermissions() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setPermissions("READ,WRITE");
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        userEntity.getPermissionList().forEach(p -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority(p);
+            authorityList.add(authority);
+        });
+        userPrincipal = new UserPrincipal(userEntity);
+
+        assertEquals(authorityList, userPrincipal.getAuthorities());
+    }
+
+    @Test
+    void getAuthoritiesRoles() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setRoles("ADMIN,GUEST");
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        userEntity.getRoleList().forEach(r -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + r);
+            authorityList.add(authority);
+        });
+        userPrincipal = new UserPrincipal(userEntity);
+
+        assertEquals(authorityList, userPrincipal.getAuthorities());
+    }
+
+    @Test
+    void expired() {
+        assertTrue(userPrincipal.isAccountNonExpired());
+    }
+
+    @Test
+    void locked() {
+        assertTrue(userPrincipal.isAccountNonLocked());
+    }
+
+    @Test
+    void credentials() {
+        assertTrue(userPrincipal.isCredentialsNonExpired());
+    }
+
+    @Test
+    void enabled() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setActive(1);
+        userPrincipal = new UserPrincipal(userEntity);
+
+        assertTrue(userPrincipal.isEnabled());
     }
 }
