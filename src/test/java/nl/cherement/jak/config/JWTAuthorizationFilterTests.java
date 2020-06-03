0b3bda8a3714b6f89016f39ea2da8e516f9d3eb2
Alex <alexiaan.vd@hotmail.com>
2020-06-03 18:24:06 +0200

    Public to private testing

--- src/test/java/nl/cherement/jak/config/JWTAuthorizationFilterTests.java
+++ src/test/java/nl/cherement/jak/config/JWTAuthorizationFilterTests.java
@@ -1,15 +1,19 @@
 package nl.cherement.jak.config;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class JWTAuthorizationFilterTests {
@@ -19,14 +23,29 @@ class JWTAuthorizationFilterTests {
     @Mock
     HttpServletRequest req;
 
-    @Test
-    void getTokenAndAuthorization() {
+    @Mock
+    private FilterChain chain;
+
+    @Mock
+    private HttpServletResponse res;
+
+    @BeforeEach void initialize(){
         req = mock(HttpServletRequest.class);
         doReturn("Bearer test").when(req).getHeader("Authorization");
         doReturn("Bearer.test").when(req).getHeader("sec-websocket-protocol");
+    }
+
+    @Test
+    void getTokenAndAuthorization() {
 
         String token = jwtAuthorizationFilter.getToken(req);
         assertEquals("test", token);
-        assertNull(jwtAuthorizationFilter.getAuthentication(token));
+    }
+    @Test
+    void doFilterInternal() throws IOException, ServletException {
+        jwtAuthorizationFilter.doFilterInternal(req,res,chain);
+        verify(req,atLeastOnce()).getHeader("Authorization");
+
+
     }
 }
